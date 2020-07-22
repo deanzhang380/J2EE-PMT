a16b218a3f7910989bccef2e6b2fff87af6690fe
TrongNghiaRyt <34794845+TrongNghiaRyt@users.noreply.github.com>
2020-07-23 00:44:08 +0700

    Authorization
    Admin user management
    Authorization for all users and URL. (need some arlert)

--- PhongMachTu/src/main/java/com/pmt/dao/IUserDao.java
+++ PhongMachTu/src/main/java/com/pmt/dao/IUserDao.java
@@ -1,9 +1,15 @@
 package com.pmt.dao;
 
+import java.util.List;
+
 import com.pmt.model.NguoiDung;
 
 public interface IUserDao extends IDao<NguoiDung>{
 
 	NguoiDung findUserByNameAndPass(String name, String pass);
 	NguoiDung findUserByID(String ID);
+	List<NguoiDung> findAll();
+	void update(int id, String hoTen, String name, String pass, int maVaiTro);
+	void insert(String hoTen, String name, String pass, int maVaiTro);
+	void delete(int id);
 }
